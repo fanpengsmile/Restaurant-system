6ae228309178f1755dea7fc5270c287d079fa5ac
fanpeng <534822577@qq.com>
2019-07-30 21:47:48 +0800

    update

--- miniprogram/pages/userConsole/userConsole.js
+++ miniprogram/pages/userConsole/userConsole.js
@@ -255,34 +255,6 @@ Page({
   },
 
   onLoad: function(options) {
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo,
-                username: JSON.parse(res.rawData).nickName
-              });
-              wx.hideLoading();
-            }
-          })
-        } else {
-          wx.hideLoading();
-          Dialog.alert({
-            message: '请先登录'
-          }).then(() => {
-            wx.switchTab({
-              url: '/pages/index/index',
-            })
-          });
-
-        }
-      }
-    })
   },
   onShow: function() {},
   onSlotChange: function(e) {
